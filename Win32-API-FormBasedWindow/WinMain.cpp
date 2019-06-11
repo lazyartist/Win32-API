611b5a72c6bd85acee3c403f3c89b8212b2b20a9
lazyartist <uuux3@yahoo.co.kr>
2019-06-12 00:26:02 +0900

    file dialog

--- Win32-API-FormBasedWindow/WinMain.cpp
+++ Win32-API-FormBasedWindow/WinMain.cpp
@@ -1,13 +1,21 @@
 #include "Windows.h"
 #include "resource.h"
+#include <stdio.h> // sprintf_s()
+#include <commdlg.h> // 없으면 GetOpenFileName() 호출 시 에러남
 
 INT_PTR CALLBACK DialogProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
+HINSTANCE g_hInstance;
+
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int CmdShow) {
+	g_hInstance = hInstance;
+
 	DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DialogProc);
+
 	return 0;
 }
 
+//char lpstrFile[MAX_PATH];
 INT_PTR CALLBACK DialogProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
@@ -22,6 +30,31 @@ INT_PTR CALLBACK DialogProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 		case IDCANCEL:
 			EndDialog(hWnd, 0);
 			break;
+		case IDC_OPEN:
+		{
+			// static 또는 전역변수로 선언하지 않으면 다이얼로그가 열리지 않음
+			static char lpstrFile[MAX_PATH];
+			// static 또는 전역변수로 선언하지 않아도 다이얼로그가 열림(강의에서는 필요하다고 함)
+			char lpstrFileTitle[MAX_PATH];
+
+			OPENFILENAME ofn = { 0, };
+			ofn.lStructSize = sizeof(OPENFILENAME);
+			ofn.hwndOwner = hWnd;
+			ofn.lpstrFilter = "Every File(*.*)\0*.*\0";
+			ofn.lpstrFile = lpstrFile;
+			ofn.lpstrFileTitle = lpstrFileTitle;
+			ofn.nMaxFile = MAX_PATH;
+			ofn.nMaxFileTitle = MAX_PATH;
+			ofn.lpstrTitle = "title";
+			ofn.lpstrInitialDir = "C:\\";
+
+			if (GetOpenFileName(&ofn)) {
+				char szResult[MAX_PATH];
+				sprintf_s(szResult, "Path: %s\n%s", ofn.lpstrFile, ofn.lpstrFileTitle);
+				MessageBox(hWnd, szResult, NULL, MB_OK);
+			};
+		}
+		break;
 		default:
 			break;
 		}
--- Win32-API-FormBasedWindow/resource.h
+++ Win32-API-FormBasedWindow/resource.h
@@ -3,6 +3,7 @@
 // Win32-API-FormBasedWindow.rc에서 사용되고 있습니다.
 //
 #define IDD_DIALOG1                     101
+#define IDC_OPEN                        1002
 
 // Next default values for new objects
 // 
@@ -10,7 +11,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
